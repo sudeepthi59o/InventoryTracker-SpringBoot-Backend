b1cb8e588ca73b4bac746d38daf95b9f5513487a
Sudeepthi <sudeepthirebbalapalli@Sudeepthis-MacBook-Air.local>
2025-10-05 11:14:58 -0400

    initial commit

---
+++ src/test/java/com/sr/inventory_tracker/InventoryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.inventory_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
